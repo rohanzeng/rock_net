2bb5c593d29c1a9ff014de431b49079747fbc478
rzeng722 <50922854+rzeng722@users.noreply.github.com>
2021-12-14 19:36:23 -0500

    Update augData.py

--- pytorch/augData.py
+++ pytorch/augData.py
@@ -1,4 +1,6 @@
-#Data augmentation file
+# Data augmentation file
+# Requires all base images to be in one folder, with all labeled images having the same name as a png in another folder
+# Loops through all base images and applies a random number of transforms on the image, then applies the same to the labeled images
 
 import numpy as np
 import os
@@ -10,6 +12,7 @@ from skimage import transform
 from skimage import util
 from skimage import io
 
+# Set of transforms
 def random_rotation(image_array):
     random_degree = random.uniform(-25, 25)
     return (sk.transform.rotate(image_array, random_degree), random_degree)
@@ -48,11 +51,13 @@ def run():
 
     #label_images = [os.path.join(label_folder_path, f) for f in os.listdir(label_folder_path) if os.path.isfile(os.path.join(label_folder_path, f))]
 
+    # Number of files that have already been generated and the total number of augmented files you desire
     num_generated_files = 5530
     num_files_desired = 50000
     total_files = len(train_images)
     #assert(len(train_images) == len(label_images))
 
+    # Places available augmentations in a dictionary
     available_tfs = {
         'rotate': random_rotation,
         'noise': random_noise,
@@ -62,9 +67,12 @@ def run():
         'shift': random_shift
         }
 
+    # Main generation loop
     while num_generated_files <= num_files_desired:
         #train_to_transform, ind = next(iter(loaders['train']))
         #label_to_transform = datasets['train_label'][ind][0]
+        
+        # Randomly selects a base training image then pulls the corresponding labeled image using the name
         image_ind = random.randint(0, total_files-1)
         train_path = train_images[image_ind]
         #label_path = label_images[image_ind]
@@ -73,6 +81,7 @@ def run():
         train_to_transform = sk.io.imread(train_path)
         label_to_transform = sk.io.imread(label_path)
 
+        # Modify this to adjust the minimum and maximum number of transforms you wish to apply
         num_tfs_to_apply = random.randint(1, 5)
 
         num_tfs = 0
@@ -86,6 +95,8 @@ def run():
                 (tf_train, shift_x, shift_y) = available_tfs[key](train_to_transform)
             else:
                 tf_train = available_tfs[key](train_to_transform)
+                
+            # Apply only affine transforms to the corresponding labeled image
             if key != 'noise' and key != 'gaussian':
                 if key == 'rotate':
                     tf_label = set_rotation(label_to_transform, deg)
@@ -99,6 +110,7 @@ def run():
         new_train_path = '%s/aug_train/augment_train_%s.png' % (folder_path, num_generated_files)
         new_label_path = '%s/aug_label/augment_label_%s.png' % (folder_path, num_generated_files)
 
+        # Save the generated images
         if type(tf_train) == np.ndarray and type(tf_label) == np.ndarray:
             io.imsave(new_train_path, tf_train)
             io.imsave(new_label_path, tf_label)
